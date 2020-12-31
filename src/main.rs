caa61a428926cdb25c2629b7c39ca6bc3ac73155
Adam Gausmann <agausmann@fastmail.com>
2020-12-30 20:50:48 -0600

    Add simulation code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use bevy::prelude::*;
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_system(tick.system())
+        .run();
+}
+
+struct TickTimer(Timer);
+
+struct Wire {
+    inputs: HashSet<Entity>,
+    state: bool,
+}
+
+struct FlipFlop {
+    inputs: HashSet<Entity>,
+    flip: bool,
+    output: bool,
+}
+
+fn tick(
+    time: Res<Time>,
+    mut timer: ResMut<TickTimer>,
+    mut flipflops: Query<&mut FlipFlop>,
+    mut wires: Query<&mut Wire>,
+) {
+    if !timer.0.tick(time.delta_seconds()).just_finished() {
+        return;
+    }
+
+    for mut ff in flipflops.iter_mut() {
+        ff.output = ff.flip
+            ^ ff.inputs
+                .iter()
+                .map(|&entity| wires.get_mut(entity).unwrap().state)
+                .fold(false, |acc, state| acc | state);
+    }
+
+    for mut wire in wires.iter_mut() {
+        wire.state = wire
+            .inputs
+            .iter()
+            .map(|&entity| flipflops.get_mut(entity).unwrap().output)
+            .fold(false, |acc, state| acc | state);
+    }
 }
